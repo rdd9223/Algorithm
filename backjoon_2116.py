b236f40474b906b65311a4a9fa8359e6b138e3fb
YeongWoooo <rdd9223@gmail.com>
2021-05-05 18:22:31 +0900

    [성공] 재귀가 아닌 반복문으로 설계

--- backjoon_2116.py
+++ backjoon_2116.py
@@ -1,45 +1,32 @@
 # 주사위 쌓기
 import sys
-sys.setrecursionlimit(100000)
 
 input = sys.stdin.readline
 
 size = int(input())
 diceList = []
 maximumAmount = 0
+pair = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
 
 for i in range(size):
     line = list(map(int, input().split()))
     diceList.append(line)
 
-
-def putDice(bottom, s=0, floorIdx=0):
-    if floorIdx == size:
-        global maximumAmount
-        if maximumAmount < s:
-            maximumAmount = s
-        return
-    else:
-        idx = diceList[floorIdx].index(bottom)
-        floor = diceList[floorIdx]
-        if idx == 0 or idx == 5:
-            s += max(floor[1], floor[2], floor[3], floor[4])
-            floorIdx += 1
-            putDice(floor[5 if idx == 0 else 0], s, floorIdx)
-            return
-        elif idx == 1 or idx == 3:
-            s += max(floor[0], floor[2], floor[5], floor[4])
-            floorIdx += 1
-            putDice(floor[3 if idx == 1 else 1], s, floorIdx)
-            return
-        elif idx == 2 or idx == 4:
-            s += max(floor[0], floor[1], floor[3], floor[5])
-            floorIdx += 1
-            putDice(floor[4 if idx == 2 else 2], s, floorIdx)
-            return
-
-
-for i in diceList[0]:
-    putDice(i)
+for i in range(6):
+    result = []
+    tmp = [1, 2, 3, 4, 5, 6]
+    tmp.remove(diceList[0][i])
+    pairNum = diceList[0][pair[i]]
+    tmp.remove(pairNum)
+    result.append(max(tmp))
+    for j in range(1, size):
+        tmp = [1, 2, 3, 4, 5, 6]
+        tmp.remove(pairNum)
+        pairNum = diceList[j][pair[diceList[j].index(pairNum)]]
+        tmp.remove(pairNum)
+        result.append(max(tmp))
+    result = sum(result)
+    if maximumAmount < result:
+        maximumAmount = result
 
 print(maximumAmount)
