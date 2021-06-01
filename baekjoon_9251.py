c321dca4523b9369b53034e588fc4049abb4b0db
YeongWoooo <rdd9223@gmail.com>
2021-06-01 16:31:16 +0900

    [성공] DP를 이용한 LCS

--- baekjoon_9251.py
+++ baekjoon_9251.py
@@ -11,13 +11,10 @@ sLen = len(second)
 
 table = [[0] * (sLen + 1) for _ in range(fLen + 1)]
 
-for i in range(fLen + 1):
-    for j in range(sLen + 1):
-        if i == 0 or j == 0:
-            table[i][j] = 0
-
-        elif first[i - 1] == second[j - 1]:
-            table[i][j] = table[i-1][j] + 1
+for i in range(1, fLen + 1):
+    for j in range(1, sLen + 1):
+        if first[i - 1] == second[j - 1]:
+            table[i][j] = table[i-1][j-1] + 1
 
         else:
             table[i][j] = max(table[i - 1][j], table[i][j-1])
