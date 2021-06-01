a0ef5a60963651e7e86697c0e56b4dddc4605096
YeongWoooo <rdd9223@gmail.com>
2021-06-01 16:10:58 +0900

    [실패] 틀렸습니다.

--- baekjoon_9251.py
+++ baekjoon_9251.py
@@ -1,30 +1,25 @@
 # Longest Common Subsequence
 import sys
 
-sys.setrecursionlimit(10**6)
-memory = []
 input = sys.stdin.readline
 
 first = input().rstrip()
 second = input().rstrip()
 
+fLen = len(first)
+sLen = len(second)
 
-def lcs(x, y):
-    # x와 y를 이미 계산했는지 메모리를 뒤져서 검사함. 있으면 함수 종료
-    if [x, y] in memory or [y, x] in memory or len(x) == 0 or len(y) == 0:
-        return 0
+table = [[0] * (sLen + 1) for _ in range(fLen + 1)]
 
-    # x와 y의 마지막 알파벳이 같다면 둘다 하나씩 깎고 length를 1 증가시키고 lcs돌림, memory에 저장
-    if x[-1] == y[-1]:
-        memory.append([x, y])
+for i in range(fLen + 1):
+    for j in range(sLen + 1):
+        if i == 0 or j == 0:
+            table[i][j] = 0
 
-        return 1 + lcs(x[: -1], y[: -1])
+        if first[i - 1] == second[j - 1]:
+            table[i][j] = table[i-1][j] + 1
 
-    # x와 y의 마지막 알파벳이 같지 않다면 왼쪽, 오른쪽을 각각 하나씩 깎고 lcs를 돌림, memory에 저장
-    else:
-        memory.append([x, y])
+        else:
+            table[i][j] = max(table[i - 1][j], table[i][j-1])
 
-        return max(lcs(x[: -1], y), lcs(x, y[: -1]))
-
-
-print(lcs(first, second))
+print(table[fLen][sLen])
