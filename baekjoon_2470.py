a3023088539e4799036ab5a10d80153386e20a29
YeongWoooo <rdd9223@gmail.com>
2021-07-18 11:35:19 +0900

    [실패] 틀렸습니다.

---
+++ baekjoon_2470.py
@@ -0,0 +1,34 @@
+# 두 용액
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+solutions = list(map(int, input().split()))
+
+solutions.sort()
+minimum = 0
+answerLeft = 0
+answerRight = N - 1
+left = 0
+right = N - 1
+
+while left < right:
+    tmp = solutions[left] + solutions[right]
+
+    if abs(tmp) < abs(minimum):
+        minimum = tmp
+        answerLeft = left
+        answerRight = right
+
+    if tmp == 0:
+        minimum = 0
+        answerLeft = left
+        answerRight = right
+        break
+    elif tmp > 0:
+        right -= 1
+    elif tmp < 0:
+        left += 1
+
+print(solutions[answerLeft], solutions[answerRight])
