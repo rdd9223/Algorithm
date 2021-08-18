14a6f9d268b93a935a8e98bfb6db5e67a393fe53
YeongWoooo <rdd9223@gmail.com>
2021-08-19 01:39:39 +0900

    [성공] 플루이드-와샬 알고리즘 사용, pypy3

--- boj_1956.py
+++ boj_1956.py
@@ -4,25 +4,30 @@ import sys
 from collections import defaultdict, deque
 
 input = sys.stdin.readline
+INF = float('inf')
 
 V, E = map(int, input().split())
-paths = [list(map(int, input().split())) for _ in range(E)]
-cycle = [-1]
-graph = defaultdict(list)
-for path in paths:
-    start, end, value = path
-    graph[start].append([end, value])
+table = [[INF for _ in range(V)] for _ in range(V)]
 
+for _ in range(E):
+    start, end, cost = map(int, input().split())
+    table[start - 1][end - 1] = cost
 
-def dfs(s, discovered=[]):
-    print(discovered)
-    discovered.append(s)
-    for e, v in graph[s]:
-        if not e in discovered:
-            discovered = dfs(e, discovered)
-    return discovered
-
+result = INF
 
 for i in range(V):
-    a = dfs(i + 1)
-    print(i, a)
+    for j in range(V):
+        '''
+        원래 플로이드 워셜 알고리즘이면 있어야 하는 코드
+        하지만 사이클을 만들어야하기 때문에 생략
+        '''
+        # if i == j:
+        #     table[i][j] = 0
+        for k in range(V):
+            table[i][j] = min(table[i][j], table[i][k] + table[k][j])
+    result = min(result, table[i][i])
+
+if result == INF:
+    print(-1)
+else:
+    print(result)
