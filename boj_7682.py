cf25c1d26445ff3adb2315290d62b61baa53ff74
YeongWoooo <rdd9223@gmail.com>
2021-08-24 03:08:35 +0900

    [성공] 꽉 채워진 경우, O가 직선이 안될 경우 추가

--- boj_7682.py
+++ boj_7682.py
@@ -16,8 +16,32 @@ while True:
     oCount = line.count('O')
     dotCount = line.count('.')
 
-    if xCount < oCount or xCount > oCount + 1:
-        result.append('invalid')
+    if xCount == 5 and oCount == 4 and dotCount == 0:
+        if line[0] == line[1] and line[1] == line[2] and line[0] == 'O':
+            result.append('invalid')
+            continue
+        if line[0] == line[3] and line[3] == line[6] and line[0] == 'O':
+            result.append('invalid')
+            continue
+        if line[0] == line[4] and line[4] == line[8] and line[0] == 'O':
+            result.append('invalid')
+            continue
+        if line[1] == line[4] and line[4] == line[7] and line[1] == 'O':
+            result.append('invalid')
+            continue
+        if line[2] == line[4] and line[4] == line[6] and line[2] == 'O':
+            result.append('invalid')
+            continue
+        if line[2] == line[5] and line[5] == line[8] and line[2] == 'O':
+            result.append('invalid')
+            continue
+        if line[3] == line[4] and line[4] == line[5] and line[3] == 'O':
+            result.append('invalid')
+            continue
+        if line[6] == line[7] and line[7] == line[8] and line[6] == 'O':
+            result.append('invalid')
+            continue
+        result.append('valid')
         continue
 
     if line[0] == line[1] and line[1] == line[2]:
@@ -51,30 +75,31 @@ while True:
     if line[2] == line[4] and line[4] == line[6]:
         if line[2] == 'O' and xCount == oCount:
             result.append('valid')
+            continue
         elif line[2] == 'X' and xCount == oCount + 1:
             result.append('valid')
             continue
     if line[2] == line[5] and line[5] == line[8]:
         if line[2] == 'O' and xCount == oCount:
             result.append('valid')
+            continue
         elif line[2] == 'X' and xCount == oCount + 1:
             result.append('valid')
             continue
     if line[3] == line[4] and line[4] == line[5]:
         if line[3] == 'O' and xCount == oCount:
             result.append('valid')
+            continue
         elif line[3] == 'X' and xCount == oCount + 1:
             result.append('valid')
             continue
     if line[6] == line[7] and line[7] == line[8]:
         if line[6] == 'O' and xCount == oCount:
             result.append('valid')
+            continue
         elif line[6] == 'X' and xCount == oCount + 1:
             result.append('valid')
             continue
-    if xCount == 5 and oCount == 4:
-        result.append('valid')
-        continue
     result.append('invalid')
 
 for txt in result:
